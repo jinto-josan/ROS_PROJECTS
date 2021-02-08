7a5f8804353ba0ed8763b3a6e86f2b5a601311e5
Jinto <jintojosan@gmail.com>
2021-02-08 17:13:00 +0530

    my_robot package added for ros1

--- gazebo/gazebo_task1/run.sh
+++ gazebo/gazebo_task1/run.sh
@@ -1,4 +1,4 @@
 export GAZEBO_PLUGIN_PATH=${GAZEBO_PLUGIN_PATH}:$(pwd)/build
 export GAZEBO_MODEL_PATH=${GAZEBO_MODEL_PATH}:$(pwd)/model
 #ros2 launch gazebo_launch.py 
-gzserver world/my_world.world --verbose
+#gzserver world/my_world.world --verbose
